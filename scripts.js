bbb113bba4bfa03a69217817d119edab8d75e1da
Joe <huntj93@gmail.com>
2020-06-07 21:55:15 +0100

    js function changes

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var articles = document.querySelectorAll("article")
+
+articles.forEach(function(elem) {
+    elem.addEventListener("click", function() {
+    alert("This is " + elem.dataset.name)
+    });
+});
+
+
+var alertButton = document.querySelector("#alert-button")
+// console.log(alertButton) - use these to check you've grabbed the right element
+    alertButton.addEventListener("click", function() {
+    alert("What are you doing... They were simple instructions.")
+    });
