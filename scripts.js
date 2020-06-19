f168281c9c46bc134bb53cc1ca20ffdcaa7cd8d2
GlitchHunt <huntj93@gmail.com>
2020-06-19 20:10:06 +0100

    cleaned js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,9 @@
 const addLink = document.querySelector('.add-link');
 const gametagInput = document.querySelector('.link-name');
 const gametags = document.querySelector('.gametags');
-
-console.log(gametagInput)
-
-var articles = document.querySelectorAll("article")
+const alertButton = document.querySelector("#alert-button")
+const deleteButton = document.querySelectorAll(".close-btn"); 
+const articles = document.querySelectorAll("article")
 
 articles.forEach(function(elem) {
     elem.addEventListener('click', function() {
@@ -12,62 +11,42 @@ articles.forEach(function(elem) {
     });
 });
 
-
-var alertButton = document.querySelector("#alert-button")
-    alertButton.addEventListener("click", function() {
+alertButton.addEventListener("click", function() {
     alert("What are you doing... They were simple instructions.")
     });
+  
 
-
-var deleteButton = document.querySelectorAll(".close-btn"); 
-console.log(deleteButton)
-// grabbing all buttons   
-        deleteButton.forEach(button =>
-            button.addEventListener('click', handleDeleteArticle)
-          );
-// assigning all buttons event listener to wait for "click"
+deleteButton.forEach(button =>
+        button.addEventListener('click', handleDeleteArticle)
+              );
 
 function handleDeleteArticle(event) {
-
     var buttonD = event.currentTarget;
     console.log(buttonD);
-    // grabbing button
     var deletedArticle = buttonD.closest('.profileCard');
     console.log(deletedArticle)
-    // grabbing closest "profileCard" to the button
     deletedArticle.remove();
-    // removes article of button clicked
-}
-
-function handleCardButtonClick(event) {
-    event.preventDefault();
-    // const validStreamerInput = streamerInput.value;
-    const validGametagInput = gametagInput.value;
-
-    console.log(validGametagInput)
-
-    if (validGametagInput) {
-        const newGametagArticle = `
-        <article class="profileCard" data-name="gametagInput">
-            <button class="close-btn" type="button">X</button>
-            <h2>${validGametagInput}</h2>
-            <p> API NEEDED</p>
-        `;
-        gametags.insertAdjacentHTML("beforeend", newGametagArticle);
-    }
 }
 
 addLink.addEventListener('click', handleCardButtonClick);
 
+function handleCardButtonClick(event) {
+    event.preventDefault();
+    const validGametagInput = gametagInput.value;
+if (validGametagInput) {
+    const newGametagArticle = `
+        <article class="profileCard" data-name="gametagInput">
+            <button class="close-btn" type="button"></button>
+            <h2>${validGametagInput}</h2>
+            <p> API NEEDED</p>
+        `;
+        
+        gametags.insertAdjacentHTML("beforeend", newGametagArticle);
 
-//     if (validStreamerInput) {
-//         const newStreamDetails = `
-//         <div class="streamer">
-//         <p class="streamerName">${validStreamerInput}</p>
-//         <a href="https://twitch.tv/${validStreamerInput}" target="_blank">Twitch</a>
-//     </div>
-//         `;
-//         streamers.insertAdjacentHTML("beforeend", newStreamDetails);    
-//     }
+        const updatedDeleteButtons = document.querySelectorAll(".close-btn");
+        const justAdded = updatedDeleteButtons[updatedDeleteButtons.length - 1];
+        console.log({ justAdded })
 
-// 
+        justAdded.addEventListener('click', handleDeleteArticle)
+}
+}
